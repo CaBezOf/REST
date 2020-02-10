1359c1a556d788056b23fb36a177d8f683097e15
caio de oliveira bezerra <caiooliverbez@hotmail.com>
2020-02-10 19:36:21 -0300

    Definindo o Body e tratando CORS

--- routes/produtos.js
+++ routes/produtos.js
@@ -4,14 +4,21 @@ const router = express.Router();
 //RETORNA TODOS OS PRODUTOS
 router.get('/', (req, res, next) => {
     res.status(200).send({
-        mensagem: 'Retorna todod os produtos'
+        mensagem: 'Retorna todos os produtos'
     });
 });
 
 //INSERE UM PRODUTO
 router.post('/', (req,res,next) => {
+
+    const produto = {
+        nome: req.body.nome,
+        preco: req.body.preco
+    }
+
     res.status(201).send({
-        mensagem: 'Insere um produto'
+        mensagem: 'Insere um produto',
+        produtoCriado: produto
     });
 });
 
