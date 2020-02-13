5475fd4c54c966946a20d2d56a4d008fb5df7593
caio de oliveira bezerra <caiooliverbez@hotmail.com>
2020-02-13 19:20:56 -0300

    Update produtos.js

--- routes/produtos.js
+++ routes/produtos.js
@@ -8,9 +8,24 @@ router.get('/', (req, res, next) => {
         if (error) { return res.status(500).send({ error: error}) }
         conn.query(
             'SELECT * FROM produtos;',
-            (error, resultado, fields) => {
+            (error, result, fields) => {
                 if (error) { return res.status(500).send({ error: error}) }
-                return res.status(200).send({response: resultado})
+                const response = {
+                    quantidade: result.length,
+                    produtos: result.map(prod => {
+                        return {
+                            id_produto: prod.id_produto,
+                            nome: prod.nome,
+                            preco: prod.preco,
+                            request: {
+                                tipo: 'GET',
+                                descricao: 'Retorna os detalhes de um produto',
+                                url: 'http://localhost:3000/produtos/' + prod.id_produto
+                            }
+                        }
+                    })
+                }
+                return res.status(200).send(response);
             }
         )
     });
@@ -23,14 +38,24 @@ router.post('/', (req,res,next) => {
         conn.query(
             'INSERT INTO produtos (nome, preco) VALUES (?,?)',
             [req.body.nome, req.body.preco],
-            (error, resultado, field) => {
+            (error, result, field) => {
                 conn.release();
 
                 if (error) { return res.status(500).send({ error: error}) }
-                res.status(201).send({
+                const response = {
                     mensagem: 'Produto inserido com sucesso',
-                    id_produto: resultado.insertId
-                });
+                    produtoCriado:{
+                        id_produto: result.id_produto,
+                        nome: req.body.nome,
+                        preco: req.body.preco,
+                        request: {
+                            tipo: 'GET',
+                            descricao: 'Retorna todos os produtos',
+                            url: 'https://localhost:3000/produtos' 
+                        }
+                    }
+                }
+                return res.status(200).send(response);
             }
         )
     });
@@ -43,9 +68,28 @@ router.get('/:id_produto', (req,res,next) => {
         conn.query(
             'SELECT * FROM produtos WHERE id_produto = ?;',
             [req.params.id_produto],
-            (error, resultado, fields) => {
+            (error, result, fields) => {
                 if (error) { return res.status(500).send({ error: error}) }
-                return res.status(200).send({response: resultado})
+                
+                if (resultado.length == 0) {
+                    return res.status(404).send({
+                        mensagem: 'Não foi encontrado produto com esse ID'
+                    })
+                }
+                
+                const response = {
+                    produto:{
+                        id_produto: result[0].id_produto,
+                        nome: result[0].nome,
+                        preco: result[0].preco,
+                        request: {
+                            tipo: 'GET',
+                            descricao: 'Retorna um produto',
+                            url: 'https://localhost:3000/produtos' 
+                        }
+                    }
+                }
+                return res.status(200).send({response: result})
             }
         )
     });    
@@ -65,10 +109,24 @@ router.patch('/', (req,res,next) => {
                 req.body.preco, 
                 req.body.id_produto
             ],
-            (error, resultado, field) => {
+            (error, result, field) => {
                 conn.release();
-
                 if (error) { return res.status(500).send({ error: error}) }
+                const response = {
+                    mensagem: 'Produto atualizado com sucesso',
+                    produtoAtualizado:{
+                        id_produto: req.body.id_produto,
+                        nome: req.body.nome,
+                        preco: req.body.preco,
+                        request: {
+                            tipo: 'GET',
+                            descricao: 'Retorna os detalhes de um produto',
+                            url: 'http://localhost:3000/produtos/' + prod.id_produto
+                        }
+                    }
+                }
+                return res.status(200).send(response);
+            }
                 res.status(202).send({
                     mensagem: 'Produto alterado com sucesso',
                     
@@ -84,7 +142,7 @@ router.delete('/', (req,res,next) => {
         if (error) { return res.status(500).send({ error: error}) }
         conn.query(
             `DELETE FROM produtos WHERE id_produto = ?`, [req.body.id_produto],
-            (error, resultado, field) => {
+            (error, result, field) => {
                 conn.release();
 
                 if (error) { return res.status(500).send({ error: error}) }
